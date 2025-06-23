ddc9024b9f0312d52fb9b2cedbc9d3d82b2e0502
Beatriz Mantovani <145379861+BeatrizMant@users.noreply.github.com>
2025-06-23 17:33:34 -0300

    Add files via upload

---
+++ conexão-dbProdutos/src/DAO/produtos/buscar_produto.js
@@ -0,0 +1,35 @@
+const {conexao} = require('../conexao.js')
+
+
+async function buscarProdutos(){
+  console.log('DAO de CATEGORIA')
+    const sql = `SELECT * FROM tbl_produtos;`
+    
+    const conn = await conexao()
+    try {
+        // Executar a consulta
+        const [rows, fields] = await conn.query(sql);
+        await conn.end()
+        return rows
+      } catch (err) {
+        return err.message
+      }
+}
+
+async function buscarProduto(codigo){
+    const sql = `SELECT * FROM tbl_produtos WHERE codigo = ?`
+    
+    const conn = await conexao()
+    
+    try {
+        // Executar a consulta
+        const [rows, fields] = await conn.query(sql, [codigo]);
+        await conn.end()
+        return rows
+      } catch (err) {
+        return err.message
+      }
+}
+
+
+module.exports = {buscarProduto, buscarProdutos}
---
+++ conexão-dbProdutos/src/DAO/produtos/inserir_produto.js
@@ -0,0 +1,20 @@
+const {conexao} = require('../conexao.js')
+
+async function incluirProduto(infos) {
+    const sql = `INSERT INTO tbl_produtos (codigo, nome, id_categoria, preco) VALUES (?,?,?,?)`
+    const conn = await conexao()
+
+    try {
+        const [results] = await conn.query(sql, infos)
+        await conn.end()
+        return results
+    } catch (err) {
+        return err.message
+    }
+}
+
+module.exports = {incluirProduto}
+
+
+
+
